12ab6fdb79ccc2f176a239998d6f60205269e4f7
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2020-09-30 21:27:37 -0400

    Update 0941. Valid Mountain Array - Arrays 101.py
    Updated Detailed Version

--- 0941. Valid Mountain Array - Arrays 101.py
+++ 0941. Valid Mountain Array - Arrays 101.py
@@ -27,3 +27,50 @@ Output: true
 
 https://leetcode.com/problems/valid-mountain-array/discuss/201958/python-easy
 """
+
+"""
+Detailed Version: Sep-30 2020
+Joseph Yu
+"""
+
+class Solution(object):
+    def validMountainArray(self, A):
+
+        # Set total length of array as N
+        N = len(A)
+
+        # We use A[i] == Max as the relative position to represent peak
+        # We should increment i along the walk up
+
+        i = 0  # starting position
+
+        # walk up the mountain
+        # subtract "relative position" to represent position of i vs. i+1 as walk up
+
+        while i+1 < N and A[i] < A[i+1]:
+            i += 1  # increment i by 1 until the peak position
+
+        # peak can NOT be the very first or very last
+        # NOTE: position of i == 4 is the No.5th element
+        # We need to compensate i by 1 >> i + 1
+
+        # if i == 0 or i+1 == N means i is the very 1st and last item
+        # Which should return False
+
+        if i == 0 or i + 1 == N:
+            return False
+
+        # if neither the 1st or last, then we should consider to walk down
+        # 🎯GOAL: check if all down-hill items are "strictly down"
+        # NOTE this still works in parallel with ALL above statement
+
+        while i+1 < N and A[i] > A[i+1]:
+            i += 1
+
+        # 😎 This final return implies we've finished and exhausted the downhill list
+        # 🎯Goal: to return i == N-1 as True
+        # and it should be the second to the last item >> True
+        # E.g. {i = 7} == {N-1 = 8 - 1 = 7}
+        # [0, 1, 2, 3, 4, 5, 6, 7]
+
+        return i == N-1
